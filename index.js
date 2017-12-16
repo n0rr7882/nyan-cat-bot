0f6f90844cec7d3bd52c008afd3e32bd04ab7f0d
N0rR <n0rr7882@gmail.com>
2017-12-16 21:35:41 +0900

    next cafeteria menu

--- index.js
+++ index.js
@@ -31,14 +31,9 @@ bot.on(/(중고|알아봐|알아봐줘) (.+)/, async (msg, props) => {
 });
 
 bot.on(/(오늘|내일|모레)?의?\s?(급식|점심(밥?))/, async (msg, props) => {
-    let now = undefined;
-    if (props.match[1] === '내일') {
-        now = moment(new Date).tz('Asia/Seoul').date(new Date().getDate() + 1);
-    } else if (props.match[1] === '모레') {
-        now = moment(new Date).tz('Asia/Seoul').date(new Date().getDate() + 2);
-    } else {
-        now = moment(new Date).tz('Asia/Seoul').date();
-    }
+    let now = now = moment(new Date).tz('Asia/Seoul').date();
+    if (props.match[1] === '내일') now += 1;
+    else if (props.match[1] === '모레') now += 2;
     const cafeteriaInfo = await autochat.getCafeteria('B100000662', now);
     msg.reply.text(cafeteriaInfo);
 });
